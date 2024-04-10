ac118d7f0d73271afbf86636862267e8fbb54dc5
Martins Michael <58573844+Mich-Martins@users.noreply.github.com>
2024-04-10 13:03:14 +0100

    Initial Commit
    current working file

---
+++ sendmail.php
@@ -0,0 +1,43 @@
+<?php
+// Variables
+$name = trim($_POST['name']);
+$subject = trim($_POST['subject']);
+$email = trim($_POST['email']);
+$website = trim($_POST['website']);
+$message = trim($_POST['message']);
+
+// Email address validation - works with php 5.2+
+function is_email_valid($email) {
+	return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+
+if( isset($name) && isset($email) && isset($message) && is_email_valid($email) ) {
+
+	// Avoid Email Injection and Mail Form Script Hijacking
+	$pattern = "/(content-type|bcc:|cc:|to:)/i";
+	if( preg_match($pattern, $name) || preg_match($pattern, $email) || preg_match($pattern, $message) ) {
+		exit;
+	}
+
+	// Email will be send
+	$to = "codepassenger@gmail.com"; // Change with your email address
+	// HTML Elements for Email Body
+	$body = <<<EOD
+	<strong>Name:</strong> $name <br>
+	<strong>Email:</strong> <a href="mailto:$email?subject=feedback" "email me">$email</a> <br> <br>
+	<strong>Website:</strong> $website <br>
+	<strong>Message:</strong> $message <br>
+EOD;
+//Must end on first column
+	
+	$headers = "From: $name <$email>\r\n";
+	$headers .= 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+	// PHP email sender
+	mail($to, $subject, $body, $headers);
+}
+
+
+?>
